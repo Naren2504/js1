9964f4243de4f6f16dab71bf17f5a0ccddb53487
Naren karthick I <narenkaarthick777@gmail.com>
2025-08-13 11:59:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+while(true){
+var a=parseInt(prompt("Enter First Number"));
+var b=parseInt(prompt("Enter second Number"));
+var op=prompt("Enter operator");
+var res;
+
+if(op==='+'){
+    res=a+b;
+    document.getElementById('output').innerHTML+='output is '+res;
+    break;
+}
+else if(op==='-'){
+    res=a-b;
+    document.getElementById('output').innerHTML+='output is '+res;
+    break;
+}
+
+ else if(op==='*'){
+    res=a*b;
+    document.getElementById('output').innerHTML+='output is '+res;
+    break;
+}
+
+else if(op==='/'){
+    res=a/b;
+    document.getElementById('output').innerHTML+='output is '+res;
+    break;
+}
+
+else {
+  res="Invalid Operator";
+}
+}
